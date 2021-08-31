6484d47ff32d1f2587f8d0220284961678ca602e
Dolos <62858251+EPIC-dev-FAIL@users.noreply.github.com>
2021-08-31 11:10:45 +1000

    Initial Commit

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODgxODMyNzk0MDk4MTA2NDA5.YSykyQ.kCYXK_BLK9AtUxk5FXMIosoDbtU',
+        prefix: '-',
+        activity: 'Brotherhood innit?',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
